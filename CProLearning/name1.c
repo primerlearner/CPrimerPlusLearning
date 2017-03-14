27c2554f93e7265687baaff7155caca92c55a923
simple <simple>
2017-03-14 11:31:14 +0800

    1

--- CProLearning/name1.c
+++ CProLearning/name1.c
@@ -1,43 +1,43 @@
 #include <stdio.h>	
 #include <string.h>
 
-#define NLEN 30
-struct namect
-{
-	char fname[NLEN];
-	char lname[NLEN];
-	int letters;
-};
-
-void getinfo(struct namect *);
-void makeinfo(struct namect *);
-void showinfo(const struct namect *);
-char *s_gets(char *st, int n);
-
-int main(void) {
-	struct namect person;
-	getinfo(&person);
-	makeinfo(&person);
-	showinfo(&person);
-
-	readline();
-	return 0;
-}
-
-void getinfo(struct namect * pst)
-{
-	printf("Please enter your first name.\n");
-	s_gets(pst->fname, NLEN);
-	printf("Please enter your last name.\n");
-	s_gets(pst->lname, NLEN);
-}
-
-void makeinfo(struct namect * pst)
-{
-	pst->letters = strlen(pst->fname) + strlen(pst->lname);
-}
-
-void showinfo(const struct namect * pst)
-{
-	printf("%s %s, your name contains %d letters.\n", pst->fname, pst->lname, pst->letters);
-}
+//#define NLEN 30
+//struct namect
+//{
+//	char fname[NLEN];
+//	char lname[NLEN];
+//	int letters;
+//};
+//
+//void getinfo(struct namect *);
+//void makeinfo(struct namect *);
+//void showinfo(const struct namect *);
+//char *s_gets(char *st, int n);
+//
+//int main(void) {
+//	struct namect person;
+//	getinfo(&person);
+//	makeinfo(&person);
+//	showinfo(&person);
+//
+//	readline();
+//	return 0;
+//}
+//
+//void getinfo(struct namect * pst)
+//{
+//	printf("Please enter your first name.\n");
+//	s_gets(pst->fname, NLEN);
+//	printf("Please enter your last name.\n");
+//	s_gets(pst->lname, NLEN);
+//}
+//
+//void makeinfo(struct namect * pst)
+//{
+//	pst->letters = strlen(pst->fname) + strlen(pst->lname);
+//}
+//
+//void showinfo(const struct namect * pst)
+//{
+//	printf("%s %s, your name contains %d letters.\n", pst->fname, pst->lname, pst->letters);
+//}
