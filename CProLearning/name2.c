5a8653743c2e899f90f920e24ca4591092162fa1
simple <simple>
2017-03-14 11:37:24 +0800

    1

--- CProLearning/name2.c
+++ CProLearning/name2.c
@@ -9,35 +9,38 @@ struct namect
 	int letters;
 };
 
-void getinfo(struct namect *);
-void makeinfo(struct namect *);
-void showinfo(const struct namect *);
+struct namect getinfo(void);
+struct namect  makeinfo(struct namect);
+void showinfo(const struct namect);
 char *s_gets(char *st, int n);
 
 int main(void) {
 	struct namect person;
-	getinfo(&person);
-	makeinfo(&person);
-	showinfo(&person);
+	person = getinfo();
+	makeinfo(person);
+	showinfo(person);
 
 	readline();
 	return 0;
 }
 
-void getinfo(struct namect * pst)
+struct namect getinfo(void)
 {
+	struct namect temp;
 	printf("Please enter your first name.\n");
-	s_gets(pst->fname, NLEN);
+	s_gets(temp.fname, NLEN);
 	printf("Please enter your last name.\n");
-	s_gets(pst->lname, NLEN);
+	s_gets(temp.lname, NLEN);
+	return temp;
 }
 
-void makeinfo(struct namect * pst)
+struct namect  makeinfo(struct namect info)
 {
-	pst->letters = strlen(pst->fname) + strlen(pst->lname);
+	info.letters = strlen(info.fname) + strlen(info.lname);
+	return info;
 }
 
-void showinfo(const struct namect * pst)
+void showinfo(const struct namect info)
 {
-	printf("%s %s, your name contains %d letters.\n", pst->fname, pst->lname, pst->letters);
+	printf("%s %s, your name contains %d letters.\n", info.fname, info.lname, info.letters);
 }
